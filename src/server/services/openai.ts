dc9806ab7f087b3ae18a3325f626f8cda4b6a859
Einsight <ghaithkhalil2004@gmail.com>
2024-02-03 05:31:44 -0500

    updates

--- src/server/services/openai.ts
+++ src/server/services/openai.ts
@@ -5,5 +5,6 @@ const global = globalThis as unknown as {
   openai: OpenAI | undefined;
 };
 
-export const openai = global.openai ?? new OpenAI({ apiKey: env.OPEN_AI_KEY });
+export const openai =
+  global.openai ?? new OpenAI({ apiKey: env.OPEN_AI_API_KEY });
 global.openai = openai;
