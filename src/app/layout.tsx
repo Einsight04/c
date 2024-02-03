8b02ff52bfd00213b841f22d3dcbf48df74e0a03
Einsight <ghaithkhalil2004@gmail.com>
2024-02-02 22:12:44 -0500

    auth speedrun world record

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -23,7 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <UserProvider>{children}</UserProvider>
+        </TRPCReactProvider>
       </body>
     </html>
   );
