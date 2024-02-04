60d12b8993ca9ffdbc62bd5f268e28413e666204
Einsight <ghaithkhalil2004@gmail.com>
2024-02-04 05:14:27 -0500

    updates

--- src/server/services/elevenlabs.ts
+++ src/server/services/elevenlabs.ts
@@ -1,3 +1,3 @@
 export const voiceId = "21m00Tcm4TlvDq8ikWAM"; // replace with your voice_id
 export const model = "eleven_monolingual_v1";
-export const wsUrl = `wss://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream-input?model_id=${model}stream-input?model_id=${model}?optimize_streaming_latency=1`;
+export const wsUrl = `wss://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream-input?model_id=${model}&optimize_streaming_latency=1`;
