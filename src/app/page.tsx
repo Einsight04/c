3b2fbceb272073c1e46be474d2a8609879fc6944
Antony <antonylbrussolo@gmail.com>
2024-02-04 02:32:37 -0500

    map shows user location instead of SF

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useRef, useEffect, useState } from "react";
+import { FaMicrophone } from "react-icons/fa";
 
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
@@ -18,10 +19,24 @@ export default function Home() {
   noStore();
 
   const geoControlRef = useRef<mapboxgl.GeolocateControl>(null);
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => geoControlRef.current?.trigger(), 1000);
-  }, []);
+    if (isMapLoaded) {
+      setTimeout(() => {
+        if (geoControlRef.current) {
+          geoControlRef.current.trigger();
+          console.log("Geolocating");
+        }
+      }, 1000);
+    }
+  }, [isMapLoaded]);
+  
+  // In your Map component's onLoad event handler
+  const handleMapLoad = () => {
+    setIsMapLoaded(true);
+    // Other map initialization code
+  };
 
   return (
     <div className="flex h-screen flex-col">
@@ -29,12 +44,16 @@ export default function Home() {
         <Map
           mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_API_KEY}
           initialViewState={{
+            // longitude: geoControlRef.current?.position?.coords.longitude,
+            // latitude: geoControlRef.current?.position?.coords.latitude,
+            // zoom: 14,
             longitude: -122.4,
             latitude: 37.8,
             zoom: 14,
           }}
+          onLoad={handleMapLoad}
           style={{ width: "100%", height: "100%" }}
-          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapStyle="mapbox://styles/mapbox/dark-v11"
         >
           <GeolocateControl
             ref={geoControlRef}
@@ -44,12 +63,15 @@ export default function Home() {
         </Map>
       </div>
       <button
-        className="flex h-1/3 items-center justify-between rounded-tl-3xl rounded-tr-3xl bg-slate-800 p-4 text-2xl text-white"
+        className="absolute bottom-[-1rem] left-0 right-0 z-10 flex h-1/3 items-center justify-center rounded-tl-3xl rounded-tr-3xl bg-gray-950 p-4 text-2xl text-white"
         onClick={() => {
           /* Define your onClick event handler here */
         }}
       >
-        {"Hold to speak"}
+        <FaMicrophone
+          className="mr-2 h-6 w-6" // Adjust the size as necessary
+        />
+        Hold to Prompt
       </button>
     </div>
   );
